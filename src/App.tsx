9fb8060ad77295d5be8c874617c903ea7e1b6943
Adithya-Vajrala <vajrlaadithya@gmail.com>
2026-07-02 17:41:39 +0530

    feat:Object passing example completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Child from './Child'
 import Addtion from './Addtion'
+import EmployeeCard from './EmployeeCard';
 
 function App() {
+  const emp = {
+    Name:"adithya",
+    id:123,
+    salary:1234453,
+    city:"hyd"    
+  };
+
   return (
     <>
       <Child name = "Adithya" age={23}></Child>
       <Addtion number1={2} number2={4}></Addtion>
+      <EmployeeCard emp={emp}></EmployeeCard>
     </>
   )
 }
--- src/EmployeeCard.tsx
+++ src/EmployeeCard.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
- type EmployeeCardProps = {
-    
+ type Employee = {
+    Name:string;
+    id:number;
+    salary:number;
+    city:string;
  }
 
-function EmployeeCard() {
+ type EmployeeCardProps = {
+    emp:Employee;
+ }
+
+function EmployeeCard({emp}:EmployeeCardProps) {
   return (
     <>
-      
+      <h1>Employee cart</h1>
+      <h2>Id:{emp.id}</h2>
+      <h2>Name:{emp.Name}</h2>
+      <h2>Salary:{emp.salary}</h2>
+      <h2>City:{emp.city}</h2>
     </>
   )
 }
