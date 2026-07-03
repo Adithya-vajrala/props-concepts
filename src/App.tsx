ef1e17e2c571dd7b44e9cad72b900ae33787baf0
Adithya-Vajrala <vajrlaadithya@gmail.com>
2026-07-03 08:08:53 +0530

    feat: render employee cards from employee array using map

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Child from './Child'
 import Addtion from './Addtion'
 import EmployeeCard from './EmployeeCard';
+import EmployeeArray from './EmployeeArray';
 
 function App() {
   const emp = {
@@ -11,11 +12,26 @@ function App() {
     city:"hyd"    
   };
 
+  const emplArray = [
+    {name:"Adithya",id:1,salary:25000,city:"VTZ"},
+    {name:"Bobby",id:2,salary:50000,city:"NRT"},
+    {name:"vinay",id:3,salary:510000,city:"Amalapuram"},
+    {name:"Anil",id:4,salary:5430000,city:"Hyderabad"},
+    {name:"pavan",id:5,salary:6750000,city:"kakainada"}
+  ]
+
   return (
     <>
       <Child name = "Adithya" age={23}></Child>
       <Addtion number1={2} number2={4}></Addtion>
       <EmployeeCard emp={emp}></EmployeeCard>
+
+      <h1>All Employeee</h1>
+      {
+        emplArray.map((empl)=>(
+          <EmployeeArray empl={empl}></EmployeeArray>
+        ))
+      }
     </>
   )
 }
